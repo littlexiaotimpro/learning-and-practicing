799ed811188de057db9670896f28eb6e6a07db3a
littlexiaotimpro <a99996655@163.com>
2020-05-30 12:42:55 +0800

    feat: create new module about spring-boot leaning

---
+++ spring-boot-start/src/main/java/com/practice/start/StartApplication.java
@@ -0,0 +1,20 @@
+package com.practice.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class StartApplication {
+
+    @RequestMapping(value = "/")
+    public String home(){
+        return "hello world";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class,args);
+    }
+}
