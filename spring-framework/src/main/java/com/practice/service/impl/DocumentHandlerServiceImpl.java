9555edcd8cc6787bcd43e078acd61a8af33cbeef
littlexiaotimpro <a99996655@163.com>
2020-08-29 20:28:28 +0800

    fix: 模块划分

---
+++ spring-framework/src/main/java/com/practice/service/impl/DocumentHandlerServiceImpl.java
@@ -0,0 +1,17 @@
+package com.practice.service.impl;
+
+import com.practice.service.DocumentHandlerService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author XiaoSi
+ * @className DocumentHandlerServiceImpl
+ * @description 文件处理接口实现
+ * @date 2020/2/6
+ */
+@Service("documentInstance")
+public class DocumentHandlerServiceImpl implements DocumentHandlerService {
+    public void uploadExcel() {
+        System.out.println("文件上传，待实现 ......");
+    }
+}
