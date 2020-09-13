7d4d80d8983274f2e032e3d9ff60aa555d997bd0
littlexiaotimpro <a99996655@163.com>
2020-09-13 13:33:06 +0800

    fix: Aware 接口功能测试

--- spring-framework-annotation/src/main/java/com/practice/config/ComponentScanConfig.java
+++ spring-framework-annotation/src/main/java/com/practice/config/ComponentScanConfig.java
@@ -17,15 +17,30 @@ import org.springframework.context.annotation.*;
 public class ComponentScanConfig {
 
     @Bean(name="demoBean",initMethod = "init",destroyMethod = "destroy")
-    @Scope("singleton")
     public DemoBean demoBean(){
         return new DemoBean();
     }
 
+    @Bean(name="demoBean1",initMethod = "init",destroyMethod = "destroy")
+    public DemoBean demoBean1(){
+        return new DemoBean();
+    }
+
+    /**
+     * 1.带参数的Bean注入，会自动去容器中获取参数类型对应的对象
+     * 默认按类型匹配，若存在多个匹配对象，则按照参数名称匹配
+     * 多实例前提下，参数名无法匹配则以类型匹配注入，此时找到多个值，属性注入失败。
+     *
+     * 2.Profile
+     * 指定组件的加载环境：spring.profiles.active=xxx
+     * 不符合条件的组件不会进行注入
+     * PS:若注解修饰整个配置类，即使内部存在符合条件的组件，但整个配置类不符合条件依旧不会注入
+     */
+    @Profile("test")
     @Bean
-    public DemoBeanE demoBeanE(DemoBean demoBean){
+    public DemoBeanE demoBeanE(DemoBean demoBean1){
         DemoBeanE demoBeanE = new DemoBeanE();
-        demoBeanE.setDemoBean(demoBean);
+        demoBeanE.setDemoBean(demoBean1);
         return demoBeanE;
     }
 }
--- spring-framework-annotation/src/test/java/com/practice/test/AnnotationConfigurationTest.java
+++ spring-framework-annotation/src/test/java/com/practice/test/AnnotationConfigurationTest.java
@@ -1,8 +1,8 @@
 package com.practice.test;
 
 import com.practice.config.ComponentScanConfig;
+import com.practice.config.aware.DemoBeanAware;
 import com.practice.entity.DemoBean;
-import com.practice.entity.DemoBeanE;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -11,17 +11,21 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class AnnotationConfigurationTest {
 
     @Test
-    public void test(){
+    public void test() {
         ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(ComponentScanConfig.class);
         printBean(context);
-        DemoBean bean = context.getBean(DemoBean.class);
+        DemoBeanAware beanAware = context.getBean(DemoBeanAware.class);
+        DemoBean bean = context.getBean("demoBean", DemoBean.class);
         String property = context.getEnvironment().getProperty("default.c-value");
-        DemoBeanE beanE = context.getBean(DemoBeanE.class);
+        String profile = context.getEnvironment().getProperty("spring.profiles.active");
+        System.out.println(property + " / " + profile);
+        // 不符合Profile配置的环境无法加载
+        // DemoBeanE beanE = context.getBean(DemoBeanE.class);
         // 手动关闭容器，singleton的实例会执行其对象销毁方法
         context.close();
     }
 
-    private void printBean(ApplicationContext context){
+    private void printBean(ApplicationContext context) {
         String[] beanDefinitionNames = context.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
             System.out.println(beanDefinitionName);
