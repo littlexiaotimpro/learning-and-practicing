5f8dd89a6db77caf3d9bae37b897baaf371c4d3a
littlexiaotimpro <a99996655@163.com>
2020-09-13 11:07:11 +0800

    feat: 新增 module

---
+++ practice/src/main/java/com/practice/algorithm/array/InitArray.java
@@ -0,0 +1,11 @@
+package com.practice.algorithm.array;
+
+import java.util.Arrays;
+
+public class InitArray {
+
+    public static void main(String[] args) {
+        int[] res = new int[5];
+        System.out.println(Arrays.toString(res));
+    }
+}
