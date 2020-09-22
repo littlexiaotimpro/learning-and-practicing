cb37bb47e6cf9a611b9282de72e9371fccf39d86
littlexiaotimpro <a99996655@163.com>
2020-09-22 22:48:41 +0800

    fix: MyBatis 模块默认配置功能测试

--- spring-framework-mybatis/src/main/java/com/practice/test/config/AutoConfiguration.java
+++ spring-framework-mybatis/src/main/java/com/practice/test/config/AutoConfiguration.java
@@ -4,6 +4,7 @@ import com.alibaba.druid.pool.DruidDataSource;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.boot.autoconfigure.MybatisProperties;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -23,8 +24,15 @@ import javax.sql.DataSource;
  */
 @PropertySource(value = {"classpath:/application.properties"})
 @EnableConfigurationProperties({MybatisProperties.class})
+// 启用自动配置，会加载很多无用的配置
+//@EnableAutoConfiguration
 public class AutoConfiguration {
 
+    /**
+     * 测试类配置数据源
+     * @param jdbcConfig
+     * @return
+     */
     @Bean
     public DataSource dataSource(JDBCConfig jdbcConfig){
         DruidDataSource dataSource = new DruidDataSource();
@@ -36,7 +44,7 @@ public class AutoConfiguration {
     }
 
     /**
-     * 使用自动配置（需要配置数据源）：
+     * 1.自动配置(不需要配置数据源的Bean注入和一下sqlSession注入)：
      * {@link org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration#sqlSessionFactory(DataSource)}
      * 在上述的 MyBatis 自动配置类中，如下：
      * <code>
@@ -46,6 +54,7 @@ public class AutoConfiguration {
      *       // ...
      *   }
      * </code>
+     * 2.测试类配置 sqlSession
      */
     @Bean
     public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource,MybatisProperties mybatisProperties){
@@ -54,8 +63,8 @@ public class AutoConfiguration {
         // 默认的解析路径是和 MapperScan 扫描路径对应的DAO的路径，如：
         // com.practice.test.dao.LogBeanDAO.java，对应的xml文件路径为：classpath:com/practice/test/dao/LogBeanDAO.xml
         // 如果需要启动资源配置文件中配置的 mybatis的本地资源路径，需要如下解析方式
-        sqlSessionFactoryBean.setMapperLocations(mybatisProperties.resolveMapperLocations());
-        sqlSessionFactoryBean.setTypeAliasesPackage(mybatisProperties.getTypeAliasesPackage());
+        //sqlSessionFactoryBean.setMapperLocations(mybatisProperties.resolveMapperLocations());
+        //sqlSessionFactoryBean.setTypeAliasesPackage(mybatisProperties.getTypeAliasesPackage());
         return sqlSessionFactoryBean;
     }
 
