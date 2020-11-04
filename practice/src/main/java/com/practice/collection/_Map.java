d88a69998d67e98af5efbfd65d5511feab784d5e
littlexiaotimpro <a99996655@163.com>
2020-11-04 10:20:58 +0800

    '集合类源码'

---
+++ practice/src/main/java/com/practice/collection/_Map.java
@@ -0,0 +1,13 @@
+package com.practice.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class _Map {
+
+    public static void main(String[] args) {
+        Map<String, String> aMap = new HashMap<>();
+        aMap.put("a", "b");
+        System.out.println(aMap);
+    }
+}
--- practice/src/main/java/com/practice/thread/Main.java
+++ practice/src/main/java/com/practice/thread/Main.java
@@ -1,9 +1,26 @@
 package com.practice.thread;
 
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * 主线程
  */
 public class Main {
+
+    private String b = "3";
+    private static final String A = "1";
+
+    public static void ta(){
+        // 静态方法无法直接调用实例变量
+        // b = "4";
+    }
+
+    private void te(){
+        b = "4";
+        String a = A;
+    }
 
     public static void main(String[] args) throws InterruptedException {
         // 当前主线程设置休眠
@@ -13,13 +30,18 @@ public class Main {
         OwnerThread ownerThread = new OwnerThread();
         OwnerRunnable ownerRunnable = new OwnerRunnable();
         Thread thread = new Thread(ownerRunnable);
+        ExecutorService executorService = Executors.newCachedThreadPool();
+//        ExecutorService executorService = Executors.newFixedThreadPool(10);
+//        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(ownerRunnable);
 
         // 自定义线程的逻辑操作
         ownerThread.start();
-        synchronized (ownerThread){
-            ownerThread.wait(10000);
-        }
         thread.start();
         System.out.println("主线程的执行方法");
+        List<Runnable> runnables = executorService.shutdownNow();
+        runnables.forEach(runnable -> {
+            System.out.println(runnable.toString());
+        });
     }
 }
