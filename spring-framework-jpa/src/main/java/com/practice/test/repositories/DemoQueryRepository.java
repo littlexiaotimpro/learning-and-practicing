4c370f26fdb2882443598d14eec20e9f059d6da7
littlexiaotimpro <a99996655@163.com>
2020-11-10 11:16:23 +0800

    feat: JPA 相关配置测试

--- spring-framework-jpa/src/main/java/com/practice/test/repositories/DemoQueryRepository.java
+++ spring-framework-jpa/src/main/java/com/practice/test/repositories/DemoQueryRepository.java
@@ -4,6 +4,13 @@ import com.practice.test.entity.Log;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * {@link @EnableJpaRepositories(basePackages = {"com.practice.test.repositories"})}
+ * 1.配置了扫描路径，继承了jpa的相关接口，@Repository 可以省略
+ *
+ * 2.@RepositoryDefinition(domainClass = Log.class, idClass = String.class)
+ * 没有继承相关类型时，必须加上如上注解，标识当前接口为持久层定义，并指定其绑定的实体映射及主键类型
+ */
 @Repository
 public interface DemoQueryRepository extends JpaRepository<Log, String> {
 
