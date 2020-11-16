1f25944a55e390c5ed93eddccdd385139f9f4ca5
littlexiaotimpro <a99996655@163.com>
2020-11-16 09:28:22 +0800

    feat: Create README.MD

--- spring-framework-amqp/src/main/java/com/practice/test/rabbitmq/Application.java
+++ spring-framework-amqp/src/main/java/com/practice/test/rabbitmq/Application.java
@@ -8,7 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
