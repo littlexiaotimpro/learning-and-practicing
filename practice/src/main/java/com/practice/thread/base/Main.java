c7f09321292e2f171f08bea884113f7893663c8d
littlexiaotimpro <a99996655@163.com>
2020-11-23 15:56:37 +0800

    feat: ThreadLocal Test

--- practice/src/main/java/com/practice/thread/base/Main.java
+++ practice/src/main/java/com/practice/thread/base/Main.java
@@ -1,47 +1,55 @@
 package com.practice.thread.base;
 
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 /**
  * 主线程
  */
 public class Main {
 
-    private String b = "3";
-    private static final String A = "1";
-
-    public static void ta(){
-        // 静态方法无法直接调用实例变量
-        // b = "4";
-    }
-
-    private void te(){
-        b = "4";
-        String a = A;
-    }
+    private static final ThreadLocal<String> threadLocal = new ThreadLocal<>();
 
     public static void main(String[] args) throws InterruptedException {
         // 当前主线程设置休眠
         Thread.sleep(2000);
 
+        // 自定义线程
+        class SelfThread extends Thread {
+            @Override
+            public void run() {
+                threadLocal.set("继承: " + currentThread().getName());
+                System.out.println(threadLocal.get());
+            }
+        }
+
+        Runnable rs = () -> {
+            threadLocal.set("实现: " + Thread.currentThread().getName());
+            System.out.println(threadLocal.get());
+        };
+
         // 创建线程对象
-        OwnerThread ownerThread = new OwnerThread();
-        OwnerRunnable ownerRunnable = new OwnerRunnable();
-        Thread thread = new Thread(ownerRunnable);
-        ExecutorService executorService = Executors.newCachedThreadPool();
-//        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        SelfThread selfThread = new SelfThread();
+        Thread thread = new Thread(rs);
+        ExecutorService executorService = new ThreadPoolExecutor(5, 5, 10,
+                TimeUnit.SECONDS, new ArrayBlockingQueue<>(5));
+//        ExecutorService executorService = Executors.newCachedThreadPool();
+//        ExecutorService executorService = Executors.newFixedThreadPool(5);
 //        ExecutorService executorService = Executors.newSingleThreadExecutor();
-        executorService.execute(ownerRunnable);
+        for (int i = 0; i < 5; i++) {
+            executorService.execute(() -> {
+                threadLocal.set("线程池: " + Thread.currentThread().getName());
+                System.out.println(threadLocal.get());
+            });
+        }
 
         // 自定义线程的逻辑操作
-        ownerThread.start();
+        selfThread.start();
         thread.start();
-        System.out.println("主线程的执行方法");
         List<Runnable> runnables = executorService.shutdownNow();
         runnables.forEach(runnable -> {
             System.out.println(runnable.toString());
         });
+        threadLocal.remove();
     }
 }
