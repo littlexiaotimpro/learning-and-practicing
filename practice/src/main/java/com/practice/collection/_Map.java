b78fd06a81ba8abc5e02d955dfc6b7373fba695f
littlexiaotimpro <a99996655@163.com>
2021-01-04 11:27:07 +0800

    feat:basic test

--- practice/src/main/java/com/practice/collection/_Map.java
+++ practice/src/main/java/com/practice/collection/_Map.java
@@ -1,13 +1,25 @@
 package com.practice.collection;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class _Map {
 
+    public static void entrySetTest() {
+        Map<String, List<String>> map = new HashMap<>();
+        map.put("a", null);
+        map.put("b", Arrays.asList("a", "b", "c"));
+        for (Map.Entry<String, List<String>> stringListEntry : map.entrySet()) {
+            List<String> value = stringListEntry.getValue();
+            if (value.get(0) == null) {
+                System.out.println("Has null");
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        Map<String, String> aMap = new HashMap<>();
-        aMap.put("a", "b");
-        System.out.println(aMap);
+        entrySetTest();
     }
 }
