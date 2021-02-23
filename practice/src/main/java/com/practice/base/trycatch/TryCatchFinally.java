8648eb69ac714bff8efc352f7324667e7e637baa
littlexiaotimpro <a99996655@163.com>
2021-02-23 10:07:24 +0800

    feat: Java Basis of try-catch-finally

--- practice/src/main/java/com/practice/base/trycatch/TryCatchFinally.java
+++ practice/src/main/java/com/practice/base/trycatch/TryCatchFinally.java
@@ -2,19 +2,42 @@ package com.practice.base.trycatch;
 
 public class TryCatchFinally {
 
+    /**
+     * 带返回值
+     *
+     * <p> 若 try 块中存在 return，无异常，
+     * 执行顺序：try.normal -> finally.normal -> try.return
+     *
+     * <p> 若 try-finally 块存在 return，无异常
+     * 执行顺序：try.normal -> finally.normal -> finally.return
+     *
+     * <p> 若 try-catch 块存在 return，存在异常
+     * 执行顺序：try.normal -> catch.normal -> finally.normal -> catch.return
+     *
+     * <p> 若 catch-finally 块存在 return，存在异常
+     * 执行顺序：try.normal -> catch.normal -> finally.normal -> finally.return
+     *
+     * @return int
+     */
     private static int onReturn() {
         try {
-            System.out.println("try");
+            System.out.println("try.normal");
             int i = 1 / 0;
             return 1;
         } catch (Exception e) {
-            System.out.println("catch");
+            System.out.println("catch.normal");
             return 2;
         } finally {
-            System.out.println("finally");
+            System.out.println("finally.normal");
+            return 3;
         }
     }
 
+    /**
+     * 无返回值
+     *
+     * <p> 执行顺序: try -> catch -> finally
+     */
     private static void noReturn() {
         try {
             System.out.println("try");
@@ -32,7 +55,7 @@ public class TryCatchFinally {
         System.out.println(i);
 
         // 2.验证try-catch等的执行顺序
-        noReturn();
+        //noReturn();
     }
 
 }
