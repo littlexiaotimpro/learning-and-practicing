27d181341fc7d5a74666f255700c47daf5e99374
littlexiaotimpro <a99996655@163.com>
2021-02-25 21:40:08 +0800

    feat: dp

--- practice/src/main/java/com/practice/algorithm/dp/_Subsequence.java
+++ practice/src/main/java/com/practice/algorithm/dp/_Subsequence.java
@@ -35,18 +35,39 @@ public class _Subsequence {
                     res[i] = Math.max(res[j] + 1, res[i]);
                 }
             }
-            max = Math.max(max,res[i]);
+            max = Math.max(max, res[i]);
         }
         // 输出各元素值对应位置的最优解
         System.out.println(Arrays.toString(res));
         return max;
     }
 
+    /**
+     * 保持字符相对位置不变，判断 s 字符串是否在 t 字符串中
+     *
+     * @param s 模式字符组
+     * @param t 目标字符组
+     */
+    private static boolean hasSubStr(String s, String t) {
+        if (s.length() > t.length()) {
+            return false;
+        }
+        char[] chars = s.toCharArray();
+        int cursor = 0;
+        for (int i = 0; i < t.length(); i++) {
+            if (cursor < chars.length && t.charAt(i) == chars[cursor]) {
+                cursor++;
+            }
+        }
+        return cursor == chars.length;
+    }
+
 
     public static void main(String[] args) {
-        int[] source = {1, 3, 6, 7, 9, 4, 10, 5, 6};
-        int lis = findLIS(source, source.length);
-        System.out.println("最长上升子序列长度为：" + lis);
+//        int[] source = {1, 3, 6, 7, 9, 4, 10, 5, 6};
+//        int lis = findLIS(source, source.length);
+//        System.out.println("最长上升子序列长度为：" + lis);
+        System.out.println(hasSubStr("b", "abc"));
     }
 
 }
