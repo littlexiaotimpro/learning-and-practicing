14d4ba886ccc2d72e28319382aabe7ebb1622d03
littlexiaotimpro <a99996655@163.com>
2021-04-21 16:53:27 +0800

    fix: test spring-kafka about dynamic create consumer

--- spring-framework-kafka/src/main/java/com/practice/kafka/Sender.java
+++ spring-framework-kafka/src/main/java/com/practice/kafka/Sender.java
@@ -1,14 +1,48 @@
 package com.practice.kafka;
 
 import com.practice.kafka.config.KafkaAutoConfiguration;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+
+import java.time.Duration;
+import java.util.Collections;
 
 public class Sender {
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(KafkaAutoConfiguration.class);
-        context.getBean(Sender.class).send("test", 42);
+
+        new Thread(() -> {
+            DefaultKafkaConsumerFactory<Integer, String> consumerFactory = (DefaultKafkaConsumerFactory<Integer, String>) context.getBean(ConsumerFactory.class);
+            consumerFactory.setBeanName("anotherConsumer");
+            Consumer<Integer, String> consumer = consumerFactory.createConsumer();
+            consumer.subscribe(Collections.singletonList("topic1"));
+
+            while(true) {
+                ConsumerRecords<Integer, String> records = consumer.poll(Duration.ofMillis(1000));
+                if (!records.isEmpty()) {
+                    System.out.println("**********************");
+                    System.out.println("anotherConsumer-" + records.iterator().next().value());
+                    System.out.println("**********************");
+                }
+            }
+        }).start();
+
+        Sender sender = context.getBean(Sender.class);
+        sender.send("test1", 42);
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        sender.send("test2", 43);
+
     }
 
     private final KafkaTemplate<Integer, String> template;
--- spring-framework-kafka/src/main/java/com/practice/kafka/listener/Listener.java
+++ spring-framework-kafka/src/main/java/com/practice/kafka/listener/Listener.java
@@ -7,7 +7,14 @@ public class Listener {
     @KafkaListener(id = "listen1", topics = "topic1")
     public void listen1(String in) {
         System.out.println("**********************");
-        System.out.println(in);
+        System.out.println("listen1-" + in);
+        System.out.println("**********************");
+    }
+
+    @KafkaListener(id = "listen2", topics = "topic1")
+    public void listen2(String in) {
+        System.out.println("**********************");
+        System.out.println("listen2-" + in);
         System.out.println("**********************");
     }
 
