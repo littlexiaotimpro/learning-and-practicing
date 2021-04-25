9aa88daac53ddbcb63ff2be25111bb98a5c94182
littlexiaotimpro <a99996655@163.com>
2021-04-25 14:49:26 +0800

    fix: kafka create consumer

---
+++ spring-framework-kafka/src/main/java/com/practice/kafka/Receiver.java
@@ -0,0 +1,43 @@
+package com.practice.kafka;
+
+import com.practice.kafka.config.KafkaAutoConfiguration;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Receiver {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(KafkaAutoConfiguration.class);
+        DefaultKafkaConsumerFactory<Integer, String> consumerFactory = (DefaultKafkaConsumerFactory<Integer, String>) context.getBean(ConsumerFactory.class);
+        consumerFactory.setBeanName("anotherConsumer");
+        Consumer<Integer, String> consumer = consumerFactory.createConsumer();
+        consumer.subscribe(Collections.singletonList("p_topic"));
+
+        ExecutorService service = Executors.newFixedThreadPool(6);
+        while (true) {
+            ConsumerRecords<Integer, String> records = consumer.poll(Duration.ofMillis(1000));
+            if (!records.isEmpty()) {
+                service.submit(() -> {
+                    for (ConsumerRecord<Integer, String> record : records) {
+                        System.out.println("**********************");
+                        System.out.println("当前线程:" + Thread.currentThread() + ","
+                                + "偏移量:" + record.offset() + "," + "主题:"
+                                + record.topic() + "," + "分区:" + record.partition()
+                                + "," + "获取的消息:" + record.value());
+                        System.out.println("**********************");
+                    }
+                });
+            }
+        }
+    }
+
+}
--- spring-framework-kafka/src/main/java/com/practice/kafka/listener/Listener.java
+++ spring-framework-kafka/src/main/java/com/practice/kafka/listener/Listener.java
@@ -4,14 +4,14 @@ import org.springframework.kafka.annotation.KafkaListener;
 
 public class Listener {
 
-    @KafkaListener(id = "listen1", topics = "topic1")
+    @KafkaListener(id = "listen1", topics = "p_topic")
     public void listen1(String in) {
         System.out.println("**********************");
         System.out.println("listen1-" + in);
         System.out.println("**********************");
     }
 
-    @KafkaListener(id = "listen2", topics = "topic1")
+    @KafkaListener(id = "listen2", topics = "p_topic")
     public void listen2(String in) {
         System.out.println("**********************");
         System.out.println("listen2-" + in);
